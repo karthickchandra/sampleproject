0e1fd844133da4b50c654c840d47e4ff374e1da7
karthick <karthick.chandra@gmail.com>
2016-07-23 12:51:51 -0700

    sample application

---
+++ samplewebapplication/samplewebapplicationtests/Controllers/HomeTest.cs
@@ -0,0 +1,22 @@
+﻿using samplewebapplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xunit;
+
+namespace samplewebapplicationtests.Controllers
+{
+    public class HomeTest
+    {
+        [Fact]
+        public void AboutSetsMessage()
+        {
+            HomeController homeController = new HomeController();
+            var aboutResult =  homeController.About() as ViewResult;
+            Assert.Equal("", aboutResult.ViewName);
+        }
+    }
+}
